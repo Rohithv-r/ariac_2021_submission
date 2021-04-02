04ef307e305e16de65efd29ba09d78b64f494f14
Rohith <vr.rohith01@gmail.com>
2021-04-02 20:47:24 +0530

    Initial commit stuff

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ariac_2021_submission
+
+**Commands for reference :**
+
+rosrun nist_gear gear.py -f /home/rohith/ARIAC_WS/src/ARIAC/nist_gear/config/trial_config/sample_kitting.yaml /home/rohith/ARIAC_WS/src/ariac_2021_submission/config/my_gear_conf.yaml --visualize-sensor-views
---
+++ src/first_executable.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <vector>
+
+#include <ros/ros.h>
+
+#include <nist_gear/LogicalCameraImage.h>
+#include <nist_gear/Order.h>
+#include <nist_gear/Proximity.h>
+#include <sensor_msgs/JointState.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Range.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/String.h>
+#include <std_srvs/Trigger.h>
+#include <trajectory_msgs/JointTrajectory.h>
+
+int main(){
+    std::cout << "Hello ARIAC!" << std::endl;
+}
